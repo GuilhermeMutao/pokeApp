e0a5807236c7250125dc4945989b626820b161c3
GuilhermeMutao <guilhermemutao@gmail.com>
2024-05-28 19:41:38 -0300

    fix spec ajusts and removed unecessary files

--- src/app/pages/pokemon-details/pokemon-details.page.spec.ts
+++ src/app/pages/pokemon-details/pokemon-details.page.spec.ts
@@ -1,17 +1,47 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { PokemonDetailsPageRoutingModule } from './pokemon-details-routing.module';
+import { SharedModule } from '../../shared.module';
+import { PokemonDetailsPageModule } from './pokemon-details.module';
 import { PokemonDetailsPage } from './pokemon-details.page';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
-describe('PokemonDetailsPage', () => {
-  let component: PokemonDetailsPage;
-  let fixture: ComponentFixture<PokemonDetailsPage>;
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PokemonDetailsPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+describe('PokemonDetailsPageModule', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        PokemonDetailsPageRoutingModule,
+        SharedModule,
+        PokemonDetailsPageModule,
+        HttpClientTestingModule 
+      ],
+      declarations: [PokemonDetailsPage],
+      providers: [ 
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: (key: string) => 'value', 
+              },
+            },
+            params: of({ id: 'testId' }) 
+          },
+        },
+      ],
+    }).compileComponents();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create the PokemonDetailsPage', () => {
+    const fixture = TestBed.createComponent(PokemonDetailsPage);
+    const page = fixture.componentInstance;
+    expect(page).toBeTruthy();
   });
 });
--- src/app/pages/pokemon-list/pokemon-list.page.spec.ts
+++ src/app/pages/pokemon-list/pokemon-list.page.spec.ts
@@ -1,17 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PokemonListPage } from './pokemon-list.page';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TestBed } from '@angular/core/testing'; 
 
 describe('PokemonListPage', () => {
   let component: PokemonListPage;
-  let fixture: ComponentFixture<PokemonListPage>;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PokemonListPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule],
+    });
+    component = TestBed.createComponent(PokemonListPage).componentInstance;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('checkScroll should update showScrollTopButton', () => {
+    const event = { detail: { scrollTop: 2000 } };
+    component.checkScroll(event);
+    expect(component.showScrollTopButton).toBe(true);
   });
 });
